ce244e79eadea759d32044b9003879fef9dbcc05
umang-kumar-quantiphi <39588277+umang-kumar-quantiphi@users.noreply.github.com>
2018-06-11 23:04:16 +0530

    Complete description
    Includes the python codes for the two lambda functions as well as the script to be run by the user.

--- scheduling.sh
+++ scheduling.sh
@@ -1,3 +1,24 @@
+#Python code implemented as a Lambda funtion to start an ec2 instance, invoked by a cloudwatch event 
+import boto3
+def lambda_handler(event, context):
+    id=i-0cc3fa01feb71a4dd
+    ec2=boto3.client('ec2')
+    response=ec2.start_instances(
+        InstanceIds=id)
+
+#Python code implemented as a Lambda funtion to stop an ec2 instance, invoked by a cloudwatch event 
+import boto3
+def lambda_handler(event, context):
+    id=i-0cc3fa01feb71a4dd
+    ec2=boto3.client('ec2')
+    response=ec2.stop_instances(
+        InstanceIds=id)
+
+
+
+
+#Below is the script which is to be run if the user wishes to schedule the instances manually
+
 #!/bin/bash
 id=i-0cc3fa01feb71a4dd
 status=$(aws ec2 describe-instances --instance-ids $id --query Reservations[].Instances[].State.Name --output text)
